3b3b14d644d3f5017d4bc33fac6418f26d3c36c8
xyryc <amdanikzz@gmail.com>
2026-02-15 08:32:43 +0600

    Initial Commit

---
+++ TrustMeter/TrustMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustMeterApp.swift
+//  TrustMeter
+//
+//  Created by Anik on 15/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TrustMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
